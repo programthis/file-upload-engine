af892a16a09d5a5ce206fef1f7797a1b7c5d2219
programthis <kylebachan@gmail.com>
2020-02-20 18:02:34 -0500

    Added some basic user tests

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,16 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+    setup do
+        @user = users(:one)
+    end
+
+    test "should fail if the user is not valid" do
+        user1 = @user.dup
+        assert_not user1.valid?
+	end
+
+    test "user should save" do
+        assert User.new(username: "caroline", email: "carol@gmail.com").save(validate: false)
+    end
 end
