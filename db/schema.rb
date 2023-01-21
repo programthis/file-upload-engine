d39262b91f771a1f27f694e4f9fc518ebe2ed55f
programthis <kylebachan@gmail.com>
2023-01-20 23:02:17 -0500

    Updated storage migration to rails 7

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2020_01_22_225053) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_21_040004) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -30,9 +30,16 @@ ActiveRecord::Schema[7.0].define(version: 2020_01_22_225053) do
     t.string "content_type"
     t.text "metadata"
     t.bigint "byte_size", null: false
-    t.string "checksum", null: false
+    t.string "checksum"
     t.datetime "created_at", precision: nil, null: false
+    t.string "service_name", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "active_storage_variant_records", force: :cascade do |t|
+    t.bigint "blob_id", null: false
+    t.string "variation_digest", null: false
+    t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
   create_table "settings", force: :cascade do |t|
@@ -96,4 +103,5 @@ ActiveRecord::Schema[7.0].define(version: 2020_01_22_225053) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
 end
